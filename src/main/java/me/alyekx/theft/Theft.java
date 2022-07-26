2d12efdf6e2c2b7fd0427224fafecc4c0caac327
Alex Yakimenko <alexyakimenkojr@gmail.com>
2022-07-26 17:02:57 +0300

    Right Click Event

--- src/main/java/me/alyekx/theft/Theft.java
+++ src/main/java/me/alyekx/theft/Theft.java
@@ -2,22 +2,28 @@ package me.alyekx.theft;
 
 import me.alyekx.theft.commands.EnderChestCommand;
 import me.alyekx.theft.commands.InventoryCommand;
+import me.alyekx.theft.events.PlayerRightClickEvent;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class Theft extends JavaPlugin {
+    public static Map<Player, Player> right_clicks = new HashMap<>();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         saveDefaultConfig();
 
+        // Listeners
+        if(getConfig().getBoolean("Theft.allow")) {
+            new PlayerRightClickEvent(this);
+        }
+
         // Commands
         new InventoryCommand(this);
         new EnderChestCommand(this);
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
     }
 }
---
+++ src/main/java/me/alyekx/theft/events/PlayerRightClickEvent.java
@@ -0,0 +1,36 @@
+package me.alyekx.theft.events;
+
+import me.alyekx.theft.Theft;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
+
+public class PlayerRightClickEvent implements Listener {
+
+    public PlayerRightClickEvent(Theft plugin) {
+
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onPlayerRightClick(PlayerInteractEntityEvent e) {
+        if(!e.getHand().equals(EquipmentSlot.HAND)) {
+            return;
+        }
+
+        if(!(e.getRightClicked() instanceof Player)) {
+            return;
+        }
+
+        Player clicker = e.getPlayer();
+        Player target = (Player) e.getRightClicked();
+
+        Theft.right_clicks.put(target, clicker);
+
+        clicker.openInventory(
+                target.getInventory()
+        );
+    }
+}
